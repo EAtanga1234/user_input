550db31c978c1ec94f3c7841d8b00b16070e083f
EAtanga1234 <166397008+EAtanga1234@users.noreply.github.com>
2024-04-08 15:25:41 +0300

    Add files via upload
    Initial commit

---
+++ user_input.py
@@ -0,0 +1,8 @@
+name=input('Enter your name:')
+print('You entered:',name)
+age=input('Enter your age:')
+print('You entered:',age)
+location=('Enter your location')
+print('You entered:',name)
+print('Welcome to weekly code challenge!')
+print('Hello'(name),'you are'(age),'years old and live in'(location))
